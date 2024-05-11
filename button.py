4b95e7847927c4b6d15ab8c1984556a79e796a02
fartuhmarko <145208478+fartuhmarko@users.noreply.github.com>
2024-05-11 17:25:56 +0300

    Add files via upload

--- button.py
+++ button.py
@@ -1,24 +1,24 @@
 from pygame import *
 
 class Button:
-    def __init__(self, window, screen_width, screen_height):
+    def __init__(self, window, screen_width, screen_height, onclick_function):
         self.window = window
         self.width = 100
         self.height = 50
         self.x = screen_width/2
         self.y = screen_height/2
-
-
-        font.init()
-        self.font1 = font.Font(None, 20)
+        self.onclick_function = onclick_function
+        font.init
+        self.font1 = font.Font(None,20)
+        self.rect = rect.Rect(self.x, self.y, self.width, self.height)
    
     def reset(self):
         draw.rect(self.window, (255,255,255), rect.Rect(self.x, self.y, self.width, self.height))  
         self.window.blit(self.font1.render("Exit", True,(0,0,0)),(100,100))
 
-    def click(self):
-        mouse1 = mouse.get_pos()
-        if mouse1[0] >= self.x and mouse1[0] <= self.x + self.width and mouse1[1] >= self.y and mouse1[1] <= self.y + self.height:
-            print(mouse1)
+    def click(self, event):
+        if event.type == MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.onclick_function()
 
 
--- main.py
+++ main.py
@@ -13,20 +13,25 @@ window = display.set_mode((screen_width-200,screen_height-200))
 
 game = True
 pause = False
+def stop_game():
+    global game
+    game = False
 
 background = transform.scale(image.load("background.jpg"),(screen_width,screen_height))
 clock = time.Clock()
 
-btn1 = Button(window, screen_width, screen_height)
+btn1 = Button(window, screen_width, screen_height, stop_game)
 
-coin = Sprite(x=randint(0,screen_width), y=randint(0, screen_height))
+font = font.Font(None, 30)
+
+coin = Sprite(x=randint(0,400), y=randint(0,400))
 submarine = Hero()
 
 offset_x = -500
-
+counter = 0
+water = [-screen_width, 0, screen_width]
 while game:
 
-    window.blit(background, (0,0))
     for e in event.get():
         if e.type == QUIT:
             game = False
@@ -36,21 +41,46 @@ while game:
         if e.type == MOUSEBUTTONDOWN:
             btn1.click()
 
+    window.blit(font.render(f"К-сть монет: {counter}", True,(255,255,255)), (15,10))
 
+    
+
+    for i in range(0,2):
+        water[i] += 5
+        window.blit(background, ( water[i],0))
+
+        if  water[i]> screen_width:
+             water[i] = -screen_width
+
+    
 
     if pause:
+        for e in event.get():
+            print(e)
+            btn1.click()
+
+        if e.type == QUIT:
+            game= False
+
+        if e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                pause = not pause
+            
+
         display.update()
         clock.tick(120)
         
         btn1.reset()
         btn1.click()
         continue
-
     
-    offset_x += 5
+    collide = submarine.rect.colliderect(coin.rect)
 
+    if collide:
+        counter +=1
+    
+    
 
-    window.blit(background, (offset_x,0))
     coin.reset(window)
     submarine.reset(window)
     submarine.move()
